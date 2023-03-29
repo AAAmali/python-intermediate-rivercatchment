1e46ae57e1c3f319d58af795744f162d9be35a77
AAAmali <AA.Amali@outlook.de>
2023-03-29 12:20:07 +0200

    add normalise function

--- catchment/models.py
+++ catchment/models.py
@@ -8,7 +8,12 @@ time across all sites.
 """
 
 import pandas as pd
+import numpy as np
 
+
+def data_normalise(data):
+    max_arr = np.array(np.max(data, axis=0))
+    return data / max_arr[np.newaxis, :]
 
 def read_variable_from_csv(filename):
     """Reads a named variable from a CSV file, and returns a
